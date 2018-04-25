32a7ac8e722cfb891cfaaf6e75c21f3e26606689
Lavanya Kannan <lavanyakannan@pal-nat186-43-133.itap.purdue.edu>
2018-04-25 03:47:18 -0400

    databasing errors fixed

--- RestaurantApp /app.js
+++ RestaurantApp /app.js
@@ -2,19 +2,141 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
-
 var mongoose = require("mongoose") ; 
+
+var passport = require("passport");
+var localStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var user = require("./models/user");
+
+app.use(require("express-session")({
+    secret: "i love dogs so much",
+    resave: false,
+    saveUninitialized: false
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
 mongoose.connect("mongodb://localhost/cs-trash") ;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var CURR;
+
+app.use(function(req,res,next){
+    CURR = req.user;
+    res.locals.currentUser = req.user;
+    next();
+})
+
 
 var city ;
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+//LOGOUT ROUTE
+
+app.get("/logout", function(req,res){
+
+    //res.render("login.ejs");
+    req.logout();
+    res.redirect("/");
+
+
+});
+
+
+
+
+//LOGIN ROUTES
+
+app.get("/login", function(req,res){
+
+    res.render("login.ejs");
+
+
+});
+
+
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/saved",
+    failureRedirect: "/login"
+
+} ),function(req,res){
+ 
+    });
+
+
+
+
+
+//REGISTER ROUTES
+
+
+app.get("/register", function(req,res){
+
+    res.render("register.ejs");
+
+
+});
+
+
+
+
+
+
+
+app.post("/register", function(req,res){
+
+    user.register(new user({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register.ejs');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/");
+        });
+    });
+
+
+});
+
+
+
+
+//checking if user is logged in
+function isLoggedIn(req,res,next){
+
+    if(req.isAuthenticated()){
+        return next(); 
+    }
+
+    res.render("login.ejs");
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 app.get("/", function(req,res){
 
     //console.log("logging get /");
-  res.render("home.ejs");
+    CURR = req.user; 
+  res.render("home.ejs", {currentUser: req.user});
 
 });
 
@@ -96,7 +218,7 @@ app.get("/r",function(req,res){
         ] ;
 
        
-        res.render("resultpage.ejs", {info:info}) ;
+        res.render("resultpage.ejs", {info:info, currentUser: req.user}) ;
 
 
 
@@ -109,14 +231,24 @@ app.get("/r",function(req,res){
 });
 
 //var restaurants= [ ];
+var stringForMong = "mongodb://localhost/" ;
+var finalString = stringForMong + CURR;
 
-var RestaurantSchema = new mongoose.Schema({
-    nameOfRestaurant:String 
+//mongoose.connect(finalString) ;
+
+  var RestaurantSchema = new mongoose.Schema({
+    nameOfRestaurant: String 
 }) ; 
 
 var Restaurant = mongoose.model("Restaurant" , RestaurantSchema) ;
 
-app.post("/r", function(req,res){
+app.post("/r", isLoggedIn, function(req,res){
+
+
+
+    //user.update(s)
+
+
 
     //city = req.body.cityname;
     //console.log(name);
@@ -124,7 +256,7 @@ app.post("/r", function(req,res){
   //  restaurants.push(info);
     //console.log(restaurants);
 
-    /*var newRestaurant = new Restaurant({
+/*    var newRestaurant = new Restaurant({
     nameofRestaurant: name 
     }) ;
 
@@ -138,7 +270,7 @@ app.post("/r", function(req,res){
         console.log(Restaurant) ; 
     }*/
 
-    Restaurant.create({
+   /* Restaurant.create({
         nameOfRestaurant: name
 
     }, function(err,rest){
@@ -151,7 +283,31 @@ app.post("/r", function(req,res){
 
             console.log("Saved Restaurant: ");
             console.log(rest);
-            Restaurant.find({},function(err  , listOfRestaurants){
+*/
+        console.log("userrrr: ");
+        //console.log(CURR);
+        console.log(req.user.username);
+
+
+        user.update({username : req.user.username}  , {$push: {restaurants  : name}}, function(err,numberAffected , rawResponse){
+            if(err)
+            {
+                console.error("error!!!!!") ; 
+            }
+            else
+            {
+                console.log("plz it happened") ;
+            }
+
+        }) ;
+
+
+        res.redirect("/saved");
+        //var finaluser = req.user.restaurants ;
+
+       // res.render("saved.ejs" , {finaluser : finaluser});
+
+          /*  Restaurant.find({},function(err  , listOfRestaurants){
             if(err)
             {
                 console.log("Could not print out the restaurants") ;
@@ -164,21 +320,11 @@ app.post("/r", function(req,res){
                 res.render("saved.ejs" , { listOfRestaurants : listOfRestaurants}) ;
             }
         });
-        
+        */
 
-       /* var propArray = [] ;
-        Restaurant.find().toArray(function(err, listOfRestaurants){
-            var i , count ;
-            for(i=0 , count = listOfRestaurants.length ; i<count ; i++)
-            {
-                propArray.push(new models.propertyModel(listOfRestaurants[i])) ;
-            }
-            var finallist = propArray; 
-            res.render("saved.ejs" , { finallist:finallist}) ;
-        })
-*/
-        }
-    });
+      
+       //}
+   // });
 
     
 
@@ -188,6 +334,29 @@ app.post("/r", function(req,res){
     //res.render("saved.ejs", {restaurants:restaurants});
 
 }) ;
+
+
+app.get("/saved", function(req,res){
+
+  user.find({username : req.user.username},function(err  , person){
+            if(err)
+            {
+                console.log("Could not print out the restaurants") ;
+            }
+            else
+            {
+                //console.log("List of restaurants: ");
+                console.log(person) ;
+                console.log(person[0].restaurants) ; 
+                var listOfRestaurants = person[0].restaurants;
+
+                res.render("saved.ejs" , {listOfRestaurants : listOfRestaurants}) ;
+            }
+        });
+
+});
+
+
 
     //res.redirect("/r");
     //console.log(city) ; 
